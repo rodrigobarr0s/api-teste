35e64312909a9ea22f7e828bf3bf1fb11a1fdfd3
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-04-20 16:29:10 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/cloud_api_starter/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cloud_api_starter.domain.repository;
+
+import com.cloud_api_starter.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
